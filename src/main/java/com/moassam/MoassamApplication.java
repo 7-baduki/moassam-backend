a1d14e5ade52702bbafaf114383848e8a466e471
Kimdonghwan <kimdonghwan913@gmail.com>
2026-04-27 20:02:19 +0900

    추가 : [김동환][feat] 프로젝트 초기 설정

---
+++ src/main/java/com/moassam/MoassamApplication.java
@@ -0,0 +1,13 @@
+package com.moassam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoassamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoassamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moassam/MoassamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moassam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoassamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
