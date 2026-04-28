9c0fca815356fda3cf24afb518033793dee7f217
Develop-KIM <kimdonghwan913@gmail.com>
2026-04-28 18:45:17 +0900

    추가 : [김동환][feat] User 도메인 구현

--- src/main/java/com/moassam/user/domain/User.java
+++ src/main/java/com/moassam/user/domain/User.java
@@ -5,24 +5,53 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
+
+    private static final String WITHDRAWN_NICKNAME = "탈퇴한 사용자";
 
     private Long id;
     private Provider provider;
     private String providerId;
     private String email;
     private String nickname;
+    private String profileImageUrl;
+    private LocalDateTime deletedAt;
 
-    private User(Provider provider, String providerId, String email, String nickname) {
-        this.provider = provider;
-        this.providerId = providerId;
-        this.email = email;
-        this.nickname = nickname;
+    public static User register(UserRegisterRequest request) {
+        User user = new User();
+
+        user.provider = request.provider();
+        user.providerId = request.providerId();
+        user.email = request.email();
+        user.nickname = request.nickname();
+        user.profileImageUrl = request.profileImageUrl();
+
+        return user;
     }
 
-    public static User register(Provider provider, String providerId, String email, String nickname) {
-        return new User(provider, providerId, email, nickname);
+    public void withdraw() {
+        if (isDeleted()) {
+            throw new IllegalStateException("이미 탈퇴한 회원입니다.");
+        }
+
+        this.deletedAt = LocalDateTime.now();
+        this.email = null;
+        this.nickname = WITHDRAWN_NICKNAME;
+        this.profileImageUrl = null;
+    }
+
+    public void rejoin(String email, String nickname, String profileImageUrl) {
+        this.deletedAt = null;
+        this.email = email;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public boolean isDeleted() {
+        return deletedAt != null;
     }
 }
---
+++ src/test/java/com/moassam/support/UserFixture.java
@@ -0,0 +1,24 @@
+package com.moassam.support;
+
+import com.moassam.user.domain.Provider;
+import com.moassam.user.domain.User;
+import com.moassam.user.domain.UserRegisterRequest;
+
+public class UserFixture {
+
+    public static User create() {
+        return User.register(new UserRegisterRequest(
+                Provider.KAKAO,
+                "kakao-12345",
+                "moassam@kakao.com",
+                "모아쌤",
+                "https://kakaocdn.net/profile/moassam.jpg"
+        ));
+    }
+
+    public static User createWithdrawn() {
+        User user = create();
+        user.withdraw();
+        return user;
+    }
+}
