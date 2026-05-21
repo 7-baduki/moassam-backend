2f90acb7235f526e06b98793f32a81c4f1376e5d
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-05-21 18:06:10 +0900

    fix: 관찰일지 생성 프롬프트 개선

--- src/main/java/com/moassam/observation/domain/Age.java
+++ src/main/java/com/moassam/observation/domain/Age.java
@@ -1,5 +1,17 @@
 package com.moassam.observation.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Age {
-    AGE_0, AGE_1, AGE_2, AGE_3, AGE_4, AGE_5
+    AGE_0(0),
+    AGE_1(1),
+    AGE_2(2),
+    AGE_3(3),
+    AGE_4(4),
+    AGE_5(5);
+
+    private final int value;
 }
