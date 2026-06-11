772061054e89f068e28db0d943471327f7f06bb3
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-06-11 16:41:44 +0900

    fix: 좋아요/북마크 카운트 갱신 로직 동시성 개선

--- src/main/java/com/moassam/post/application/required/BookmarkRepository.java
+++ src/main/java/com/moassam/post/application/required/BookmarkRepository.java
@@ -4,6 +4,7 @@ import com.moassam.post.domain.bookmark.PostBookmark;
 import com.moassam.post.domain.post.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
@@ -20,15 +21,37 @@ public interface BookmarkRepository extends Repository<PostBookmark, Long> {
     void delete(PostBookmark postBookmark);
 
     @Query("""
-        select p
-        from PostBookmark b
-        join Post p on p.id = b.postId
-        where b.userId = :userId
-        order by b.createdAt desc
-    """)
+                select p
+                from PostBookmark b
+                join Post p on p.id = b.postId
+                where b.userId = :userId
+                order by b.createdAt desc
+            """)
     Page<Post> findBookmarkedPostsByUserId(Long userId, Pageable pageable);
 
     long countByUserId(Long userId);
+
+    @Modifying
+    @Query(
+            value = """
+            INSERT INTO post_bookmarks (post_id, user_id, created_at, updated_at)
+            VALUES (:postId, :userId, NOW(), NOW())
+            ON CONFLICT (post_id, user_id) DO NOTHING
+            """,
+            nativeQuery = true
+    )
+    int insertIgnore(Long postId, Long userId);
+
+    @Modifying
+    @Query(
+            value = """
+                    DELETE FROM post_bookmarks
+                    WHERE post_id = :postId
+                      AND user_id = :userId
+                    """,
+            nativeQuery = true
+    )
+    int deleteByPostIdAndUserId(Long postId, Long userId);
 
     void deleteByUserId(Long userId);
 }
